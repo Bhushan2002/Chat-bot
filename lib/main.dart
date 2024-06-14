5d62367b79db9570fbc18c5e8001721277429cc1
bhushan <bwaghode321@gmail.com>
2024-06-14 12:55:26 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
+import 'package:ai_project/helper/global.dart';
+import 'package:ai_project/helper/pref.dart';
+import 'package:ai_project/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Pref.initialize();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(const MyApp());
 }
+
+getApplicationDocumentsDirectory() {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Text("hello"),
-      ),
+    return  GetMaterialApp(
+      title: appName,
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
